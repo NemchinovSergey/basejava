b13e256059dc3741be55a5cf51ec26b3bd38e09d
Nemchinov Sergey <googoodemon@gmail.com>
2019-08-17 20:11:25 +0700

    ArrayStorage implementation

--- src/main/java/ArrayStorage.java
+++ src/main/java/ArrayStorage.java
@@ -1,30 +1,68 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
 
-    void clear() {
+    private static final int CAPACITY = 10000;
+    private static final Resume[] storage = new Resume[CAPACITY];
+    private int size = 0;
+
+    public int getCapacity() {
+        return CAPACITY;
     }
 
-    void save(Resume r) {
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    Resume get(String uuid) {
+    public void save(Resume r) {
+        Objects.requireNonNull(r, "Parameter is null");
+
+        if (size >= storage.length) {
+            throw new IllegalStateException("Storage is full");
+        }
+
+        storage[size++] = r;
+    }
+
+    public Resume get(String uuid) {
+        Objects.requireNonNull(uuid, "uuid is null");
+
+        for (int i = 0; i < size; i++) {
+            Resume r = storage[i];
+            if (Objects.equals(r.getUuid(), uuid)) {
+                return r;
+            }
+        }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        Objects.requireNonNull(uuid, "uuid is null");
+
+        for (int i = 0; i < size; i++) {
+            Resume r = storage[i];
+            if (Objects.equals(r.getUuid(), uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+                storage[size] = null;
+            }
+        }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return size;
     }
+
 }
