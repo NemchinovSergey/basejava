63fbbea4db1d9fdab020fba806a3752fc085e2fe
Nemchinov Sergey <googoodemon@gmail.com>
2019-08-19 22:39:37 +0700

    implemented SortedArrayStorage class and tests

--- src/main/java/com/nsergey/basejava/storage/SortedArrayStorage.java
+++ src/main/java/com/nsergey/basejava/storage/SortedArrayStorage.java
@@ -1,6 +1,7 @@
 package com.nsergey.basejava.storage;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 import com.nsergey.basejava.model.Resume;
 
@@ -8,22 +9,36 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected int indexOf(String uuid) {
-        return Arrays.binarySearch(storage, new Resume(uuid));
-    }
-
-    @Override
-    public void save(Resume resume) {
-
+        int index = Arrays.binarySearch(getAll(), new Resume(uuid));
+        return index >= 0 ? index : NOT_FOUND;
     }
 
     @Override
     public void delete(String uuid) {
+        Objects.requireNonNull(uuid, "Resume is null");
 
+        int index = indexOf(uuid);
+        if (index == NOT_FOUND) {
+            System.out.println("Not found: " + uuid);
+        } else {
+            storage[index] = null; // to help GC
+            System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+            storage[size - 1] = null;
+            size--;
+            System.out.println("Resume deleted: " + uuid);
+        }
     }
 
     @Override
-    public void update(Resume resume) {
-
+    protected void add(Resume resume) {
+        //  http://codereview.stackexchange.com/questions/36221/binary-search-for-inserting-in-array#answer-36239
+        int index = Arrays.binarySearch(getAll(), resume);
+        if (index < 0) {
+            int pos = -index - 1;
+            System.arraycopy(storage, pos, storage, pos + 1, size - pos);
+            storage[pos] = resume;
+            size++;
+        }
     }
 
 }
--- src/test/java/com/nsergey/basejava/storage/SortedArrayStorageTest.java
+++ src/test/java/com/nsergey/basejava/storage/SortedArrayStorageTest.java
@@ -1,53 +1,139 @@
 package com.nsergey.basejava.storage;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import com.nsergey.basejava.model.Resume;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class SortedArrayStorageTest {
 
+    private static Storage ARRAY_STORAGE;
+
+    private static final int INITIAL_SIZE = 1000;
+
+    // sorted array of resumes
+    private static Resume[] resumesArr = IntStream.range(1, INITIAL_SIZE + 1)
+                                                  .mapToObj(n -> new Resume("uuid" + n))
+                                                  .toArray(Resume[]::new);
+
     @BeforeEach
     void setUp() {
-    }
-
-    @Test
-    void indexOf() {
-    }
-
-    @Test
-    void save() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-    @Test
-    void update() {
-    }
-
-    @Test
-    void get() {
-    }
-
-    @Test
-    void size() {
+        ARRAY_STORAGE = new SortedArrayStorage();
+        List<Resume> resumeList = new ArrayList<>(Arrays.asList(resumesArr));
+        Collections.shuffle(resumeList);
+        resumeList.forEach(ARRAY_STORAGE::save);
     }
 
     @Test
     void clear() {
+        assertTrue(ARRAY_STORAGE.size() > 0);
+
+        ARRAY_STORAGE.clear();
+
+        assertEquals(0, ARRAY_STORAGE.size());
     }
 
     @Test
-    void getCapacity() {
+    void save() {
+        String uuid = "some-test-uuid";
+        assertNull(ARRAY_STORAGE.get(uuid));
+
+        ARRAY_STORAGE.save(new Resume(uuid));
+
+        assertEquals(uuid, ARRAY_STORAGE.get(uuid).getUuid());
+    }
+
+    @Test
+    void get() {
+        String uuid = "uuid10";
+        assertEquals(uuid, ARRAY_STORAGE.get(uuid).getUuid());
+    }
+
+    @Test
+    void delete() {
+        String uuid = "uuid10";
+        int size = ARRAY_STORAGE.size();
+        assertNotNull(ARRAY_STORAGE.get(uuid));
+
+        ARRAY_STORAGE.delete(uuid);
+
+        assertNull(ARRAY_STORAGE.get(uuid));
+        assertEquals(size - 1, ARRAY_STORAGE.size());
     }
 
     @Test
     void getAll() {
+        Resume[] resumes = ARRAY_STORAGE.getAll();
+        assertNotNull(resumes);
+        assertEquals(ARRAY_STORAGE.size(), resumes.length);
     }
 
     @Test
-    void indexOf1() {
+    void size() {
+        assertTrue(ARRAY_STORAGE.size() > 0);
     }
+
+    @Test
+    void testNulls() {
+        Exception exception;
+
+        exception = assertThrows(NullPointerException.class, () -> ARRAY_STORAGE.save(null));
+        assertEquals("Resume is null", exception.getMessage());
+
+        exception = assertThrows(NullPointerException.class, () -> ARRAY_STORAGE.get(null));
+        assertEquals("uuid is null", exception.getMessage());
+
+        exception = assertThrows(NullPointerException.class, () -> ARRAY_STORAGE.delete(null));
+        assertEquals("Resume is null", exception.getMessage());
+    }
+
+    @Test
+    void testStorageOverflow() {
+        Executable fillStorage = () -> {
+            ARRAY_STORAGE.clear();
+            IntStream.range(0, ARRAY_STORAGE.getCapacity() + 1).forEach(n -> ARRAY_STORAGE.save(new Resume(
+                    "uuid" + n)));
+        };
+        Exception exception = assertThrows(IllegalStateException.class, fillStorage);
+        assertEquals("Storage is full", exception.getMessage());
+    }
+
+    @Test
+    void testSaveDuplicates() {
+        String uuid = "uuid10";
+        assertNotNull(ARRAY_STORAGE.get(uuid));
+
+        int size = ARRAY_STORAGE.size();
+        ARRAY_STORAGE.save(new Resume(uuid));
+        assertNotNull(ARRAY_STORAGE.get(uuid));
+        assertEquals(size, ARRAY_STORAGE.size());
+    }
+
+    @Test
+    void update() {
+        String uuid = "uuid10";
+        Resume oldResume = ARRAY_STORAGE.get(uuid);
+        assertNotNull(oldResume);
+
+        Resume newResume = new Resume(uuid);
+        ARRAY_STORAGE.update(newResume);
+
+        assertNotSame(oldResume, newResume);
+        assertSame(newResume, ARRAY_STORAGE.get(uuid));
+    }
+
 }
