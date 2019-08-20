8f45f9e6be57b3d064e0198d636405e242194f90
Nemchinov Sergey <googoodemon@gmail.com>
2019-08-20 21:36:29 +0700

    invocation toString() method via reflection

--- src/main/java/com/nsergey/basejava/MainReflection.java
+++ src/main/java/com/nsergey/basejava/MainReflection.java
@@ -1,19 +1,24 @@
 package com.nsergey.basejava;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import com.nsergey.basejava.model.Resume;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args)
+            throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println("Field name: " + field.getName());
         System.out.println("Field value: " + field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
-        System.out.println(r);
+
+        // invocation r.toString() via reflection
+        Method toStringMethod = r.getClass().getDeclaredMethod("toString");
+        System.out.println(toStringMethod.invoke(r));
     }
 }
