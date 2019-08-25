efe54c639e9fbb263ccdc308a599254640834fd9
Nemchinov Sergey <googoodemon@gmail.com>
2019-08-25 21:37:17 +0700

    MapStorage implemented

--- src/main/java/com/nsergey/basejava/storage/MapStorage.java
+++ src/main/java/com/nsergey/basejava/storage/MapStorage.java
@@ -1,51 +1,64 @@
 package com.nsergey.basejava.storage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.nsergey.basejava.model.Resume;
 
 public class MapStorage extends AbstractStorage {
 
+    private final Map<String, Resume> storage;
+
+    public MapStorage() {
+        storage = new HashMap<>();
+    }
+
+    public MapStorage(Map<String, Resume> storage) {
+        this.storage = storage;
+    }
+
     @Override
     protected Object getSearchKey(String uuid) {
-        return null;
+        return uuid;
     }
 
     @Override
     protected boolean isExist(Object key) {
-        return false;
+        return storage.containsKey((String)key);
     }
 
     @Override
     protected void doAdd(Resume resume, Object key) {
-
+        storage.put(resume.getUuid(), resume);
     }
 
     @Override
     protected Resume doGet(Object key) {
-        return null;
+        return storage.get((String)key);
     }
 
     @Override
     protected void doUpdate(Resume resume, Object key) {
-
+        storage.put(resume.getUuid(), resume);
     }
 
     @Override
     protected void doDelete(Object key) {
-
+        storage.remove((String)key);
     }
 
     @Override
     public int size() {
-        return 0;
+        return storage.size();
     }
 
     @Override
     public void clear() {
-
+        storage.clear();
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return storage.values().toArray(new Resume[0]);
     }
 }
