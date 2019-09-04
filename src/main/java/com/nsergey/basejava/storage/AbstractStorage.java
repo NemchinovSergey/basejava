1b6232043f8710cc1a6abd6dab1dbecd0e21c5f6
Nemchinov Sergey <googoodemon@gmail.com>
2019-09-04 21:54:22 +0700

    generalization of AbstractStorage

--- src/main/java/com/nsergey/basejava/storage/AbstractStorage.java
+++ src/main/java/com/nsergey/basejava/storage/AbstractStorage.java
@@ -8,24 +8,24 @@ import com.nsergey.basejava.exception.ExistStorageException;
 import com.nsergey.basejava.exception.NotExistStorageException;
 import com.nsergey.basejava.model.Resume;
 
-public abstract class AbstractStorage implements Storage {
+public abstract class AbstractStorage<T> implements Storage {
 
     protected static final Comparator<Resume> RESUME_UUID_COMPARATOR = Comparator.comparing(Resume::getUuid);
 
     protected static final Comparator<Resume> RESUME_FULL_NAME_COMPARATOR = Comparator.comparing(Resume::getFullName)
                                                                                       .thenComparing(Resume::getUuid);
 
-    protected abstract Object getSearchKey(String uuid);
+    protected abstract T getSearchKey(String uuid);
 
-    protected abstract boolean isExist(Object key);
+    protected abstract boolean isExist(T key);
 
-    protected abstract void doAdd(Resume resume, Object key);
+    protected abstract void doAdd(Resume resume, T key);
 
-    protected abstract Resume doGet(Object key);
+    protected abstract Resume doGet(T key);
 
-    protected abstract void doUpdate(Resume resume, Object key);
+    protected abstract void doUpdate(Resume resume, T key);
 
-    protected abstract void doDelete(Object key);
+    protected abstract void doDelete(T key);
 
     @Override
     public abstract int size();
@@ -40,7 +40,7 @@ public abstract class AbstractStorage implements Storage {
     public void save(Resume resume) {
         Objects.requireNonNull(resume, "Resume is null");
 
-        Object key = getSearchKey(resume.getUuid());
+        T key = getSearchKey(resume.getUuid());
         if (isExist(key)) {
             throw new ExistStorageException(resume.getUuid());
         }
@@ -52,7 +52,7 @@ public abstract class AbstractStorage implements Storage {
     public Resume get(String uuid) {
         Objects.requireNonNull(uuid, "uuid is null");
 
-        Object key = getSearchKey(uuid);
+        T key = getSearchKey(uuid);
         if (!isExist(key)) {
             throw new NotExistStorageException(uuid);
         } else {
@@ -64,7 +64,7 @@ public abstract class AbstractStorage implements Storage {
     public void update(Resume resume) {
         Objects.requireNonNull(resume, "uuid is null");
 
-        Object key = getSearchKey(resume.getUuid());
+        T key = getSearchKey(resume.getUuid());
         if (!isExist(key)) {
             throw new NotExistStorageException(resume.getUuid());
         } else {
@@ -76,7 +76,7 @@ public abstract class AbstractStorage implements Storage {
     public void delete(String uuid) {
         Objects.requireNonNull(uuid, "uuid is null");
 
-        Object key = getSearchKey(uuid);
+        T key = getSearchKey(uuid);
         if (!isExist(key)) {
             throw new NotExistStorageException(uuid);
         } else {
